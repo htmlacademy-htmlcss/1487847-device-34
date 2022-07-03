cbe78321b03fb3fa58a3f41c37a956aeae30d4a6
Keks <keks@htmlacademy.ru>
2022-07-03 20:49:42 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/script.js
@@ -0,0 +1,6 @@
+function openForm() {
+  document.querySelector('.modal-container').classList.add('open-form');
+}
+function closeForm() {
+  document.querySelector('.modal-container').classList.remove('open-form');
+}
