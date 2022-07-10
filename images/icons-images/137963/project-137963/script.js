5290c1529a8a5a822686d83b259ea3c676df3f09
OlegMikhaylovE <103054177+OlegMikhaylov0001@users.noreply.github.com>
2022-07-10 13:20:28 +0400

    delete arhive

--- images/icons-images/137963/project-137963/script.js
+++
@@ -1,6 +0,0 @@
-function openForm() {
-  document.querySelector('.modal-container').classList.add('open-form');
-}
-function closeForm() {
-  document.querySelector('.modal-container').classList.remove('open-form');
-}
